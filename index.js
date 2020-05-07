e1487490add3711d256eea6d766689b16d873208
andrew041994 <andrew041994@gmail.com>
2020-05-07 01:37:21 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+ let result = arr.find( function(win) {
+    return win.result === "W"
+  })
+  if (result) {
+    return result.year
+  }
+
+}
+
+// roommates.find( function(s) { return s === "winston" })
